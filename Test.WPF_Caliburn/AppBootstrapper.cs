2391ec28a3cda1b9799f37ed48e076c063091585
Исаченко Владимир <vv.isachenko@gmail.com>
2015-05-23 15:13:03 +0600

    Тестовое WPF решение

---
+++ Test.WPF_Caliburn/AppBootstrapper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro.Autofac;
+
+using Test.WPF_Caliburn.ViewModels;
+
+namespace Test.WPF_Caliburn
+{
+	class AppBootstrapper : AutofacBootstrapper<MainViewModel>
+	{
+		
+	}
+}
